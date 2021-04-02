1334a31079c5e158569f4aa700c7997ef110eea5
Anita Hu <anitahu113@gmail.com>
2021-04-01 23:17:49 -0400

    lane extractor wip

--- src/lane_utils.py
+++ src/lane_utils.py
@@ -6,11 +6,6 @@ from scipy.interpolate import CubicSpline
 import lane_config
 from enum import Enum
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-from matplotlib.patches import Wedge
-from matplotlib.collections import PatchCollection
 
 class Side(Enum):
     LEFT = 0
@@ -33,7 +28,7 @@ class LaneExtractor(object):
         self.lane_min_y_diff = 2
         self.lane_ver_threshold = 0.7
         self.waypoint_dist = 2.0
-        self.max_waypoint_range = 5000.0 # in cm?
+        self.max_waypoint_range = 70.0
         self.world2vehicle = None
         self.vehicle2camera = np.array(self.camera.sensor_transform.get_inverse_matrix())
         self.projection_matrix = self.camera.projection_matrix
@@ -48,52 +43,23 @@ class LaneExtractor(object):
             carla.LaneMarkingType.BottsDots,
             carla.LaneMarkingType.Curb
         ]
-
-        # Get fov and position of camera
-        sensors = self.camera.sensors
-        rgb_cam_sensor = list(filter(lambda s: s[0] == 'sensor.camera.rgb', sensors))[0]
-        rgb_cam_bp = rgb_cam_sensor[-1]
-        self.rgb_cam_fov = rgb_cam_bp.get_attribute("fov").as_float()
-        self.rgb_cam_tf = self.camera.sensor_transform
-
         # Get all waypoints in map
         map_waypoints = self.map.generate_waypoints(self.waypoint_dist)
-        # print(self.map_waypoints[0].road_id) # 259
-        # print(self.map_waypoints[0].lane_id) # 7
-        self.waypoints_pos = [[], [], []]
-        self.waypoints_road_id = []
-        self.waypoints_lane_id = []
+        self.waypoints = []
+        self.waypoints_pos = []
         for wp in map_waypoints:
-            # Remove junction waypoints
-            if wp.is_junction:
-                continue
             # Remove waypoints with irrelevant lane markings
             if wp.right_lane_marking.type not in self.lane_marking_types and wp.left_lane_marking.type not in self.lane_marking_types:
                 continue
             loc = wp.transform.location
-            self.waypoints_pos[0].append(loc.x)
-            self.waypoints_pos[1].append(loc.y)
-            self.waypoints_pos[2].append(loc.z)
-            self.waypoints_road_id.append(wp.road_id)
-            self.waypoints_lane_id.append(wp.lane_id)
+            self.waypoints.append(wp)
+            self.waypoints_pos.append([loc.x, loc.y, loc.z])
         self.waypoints_pos = np.array(self.waypoints_pos) # (3, N)
-        self.waypoints_road_id = np.array(self.waypoints_road_id) # (N,)
-        self.waypoints_lane_id = np.array(self.waypoints_lane_id) # (N,)
-        # plt.figure(figsize=(12, 8), dpi=400)
-        # plt.scatter(self.waypoints_pos[0], self.waypoints_pos[1], s=1, c=ids, cmap='gist_rainbow')
-        # plt.savefig("/home/carla/PythonAPI/simulanes/debug_plots/road_id_0_60.png")
-        # print("plot saved")
-
         
-
-    def project_to_camera(self, world_points):
-        """
-        Project all world points to camera coordinates.
-
-        world_points: (3, N) numpy array of points
-        """
-        if world_points is None:
-            return world_points
+    
+    def transform_world_to_camera(self, world_points):
+        # Points in map coords of shape (3, num_points).
+        world_points = np.array(world_points).T
         
         # Add an extra 1.0 at the end of each 3d point so it becomes of
         # shape (4, num_points) and it can be multiplied by a (4, 4) matrix.
@@ -104,9 +70,20 @@ class LaneExtractor(object):
         
         # Transform the points from vehicle coords to camera coords.
         sensor_points = np.dot(self.vehicle2camera, vehicle_points)
+        
+        return sensor_points
+        
+    def project_to_camera(self, world_points):
+        """
+        Project all world points to camera coordinates.
 
-        # Get mask for all points within the max range threshold
-        points_in_range_mask = (sensor_points[0]**2 + sensor_points[1]**2 + sensor_points[2]**2 < self.max_waypoint_range)
+        world_points: (3, N) numpy array of points
+        """
+        if world_points is None:
+            return world_points
+        
+        # Transform the points from world coords to camera coords.
+        sensor_points = self.transform_world_to_camera(world_points)
         
         # New we change from camera coords to pixel coordinates
         # ^ z                       . z
@@ -141,14 +118,11 @@ class LaneExtractor(object):
             (points_2d[:, 0] > 0.0) & (points_2d[:, 0] < self.image_dim[0]) & \
             (points_2d[:, 1] > 0.0) & (points_2d[:, 1] < self.image_dim[1]) & \
             (points_2d[:, 2] > 0.0)
-
-        # Combine masks for points in canvas and points within max range
-        cam_points_in_range_mask = (points_in_canvas_mask) & (points_in_range_mask)
             
-        points_2d = points_2d[cam_points_in_range_mask]
+        points_2d = points_2d[points_in_canvas_mask]
         points_2d = points_2d[:, :2].astype(np.int)
 
-        return points_2d, cam_points_in_range_mask
+        return points_2d
 
     def format_lane(self, lane, lane_type, lane_color):
         lane_class = 0
@@ -179,7 +153,7 @@ class LaneExtractor(object):
         ys = np.arange(int(min_y), int(max_y)+1)
         xs = cs(ys).astype(int)
         verification_points = np.vstack((ys[np.newaxis, :], xs[np.newaxis, :]))
-
+        
         if lane_type == carla.LaneMarkingType.Broken:
             lane_class = 1
         elif lane_type == carla.LaneMarkingType.Solid:
@@ -202,48 +176,38 @@ class LaneExtractor(object):
                 
         return lane_xs.tolist(), lane_class, verification_points
 
-    def get_marking_positions(self, lane_points, crossed, side):
-        lane = []
-        lane_marking_type = carla.LaneMarkingType.NONE
-        lane_marking_color = carla.LaneMarkingColor.Standard
-        for point in lane_points:
-            lane_marking_type = point.left_lane_marking.type if side == side.LEFT else point.right_lane_marking.type
-            lane_marking_color = point.left_lane_marking.color if side == side.LEFT else point.right_lane_marking.color
-            
-            # check if waypoint is in juction
-            if lane_marking_type == carla.libcarla.LaneMarkingType.NONE:
-                if len(lane) == 0:
-                    # vehicle in juction, will provide lanes after the juction
-                    continue
-                else:
-                    # vehicle approaching juction, do not provide lanes after juction
-                    lane_marking_type = lane_points[0].left_lane_marking.type if side == side.LEFT else lane_points[0].right_lane_marking.type
-                    lane_marking_color = lane_points[0].left_lane_marking.color if side == side.LEFT else lane_points[0].right_lane_marking.color
-                    break 
+    def get_marking_positions(self, waypoints, crossed, side):
+        lane_points = []
+        for point in waypoints:
             # Display forward vector
             forward_vector = point.transform.get_forward_vector()
             if (side == Side.LEFT and crossed) or (side == Side.RIGHT and not crossed):
                 # Rotate 90 deg CCW
                 rotated_vector = carla.Vector3D(-forward_vector.y, forward_vector.x, 0)
+                lane_marking_type = point.right_lane_marking.type
+                lane_marking_color = point.right_lane_marking.color
             else:
                 # Rotate 90 deg CW
                 rotated_vector = carla.Vector3D(forward_vector.y, -forward_vector.x, 0)
+                lane_marking_type = point.left_lane_marking.type 
+                lane_marking_color = point.left_lane_marking.color
             
             # Scale rotated vector to have a length equal to lane width / 2
             angle = np.arctan2(rotated_vector.y, rotated_vector.x)
             target_length = point.lane_width / 2
             new_vector = carla.Vector3D(target_length * np.cos(angle), target_length * np.sin(angle), 0)
-
+        
             start_point = point.transform.location
             end_point = start_point + new_vector
-            # self.world.debug.draw_point(end_point, size=0.1, color=carla.Color(r=0, g=255, b=0, a=255), life_time=0.1)
-            lane.append([end_point.x, end_point.y, end_point.z])
+            
+            self.world.debug.draw_point(end_point, size=0.1, color=carla.Color(r=0, g=255, b=0, a=255), life_time=0.1)
+            lane_points.append([end_point.x, end_point.y, end_point.z])
         
-        camera_coord_lane = self.project_to_camera(lane)
+        camera_coord_lane = self.project_to_camera(lane_points)
         lane_xs, lane_class, verification_points = self.format_lane(camera_coord_lane, lane_marking_type, lane_marking_color)
         
         if len(lane_xs) >= self.min_lane_points:
-            self.lanes.append({'xs': lane_xs, 'class': lane_class, 'ver_points': verification_points})
+            self.lanes.append({'xs': lane_xs, 'class': lane_class})
 
     def verify_lanes(self):
         """
@@ -283,27 +247,60 @@ class LaneExtractor(object):
             json.dump(label_dict, label_file)
 
     def visualize_lanes(self, display):
-        unique_ids = np.unique(np.hstack((self.cam_road_ids[:, np.newaxis], self.cam_lane_ids[:, np.newaxis])), axis=0)
-        hues = np.linspace(0, 360, unique_ids.shape[0])[:, np.newaxis]
-        id_hues = {}
-        for i in range(unique_ids.shape[0]):
-            id_hues[tuple(unique_ids[i])] = hues[i]
-        
-        for n in range(self.cam_pts.shape[0]):
-            ID = (self.cam_road_ids[n], self.cam_lane_ids[n])
-            hue = id_hues[ID]
-
+        hue_step = 360 // max(len(self.lanes), 1)
+        hue = 0
+        for lane_dict in self.lanes:
+            lane_xs = lane_dict['xs']
+            lane_ys = lane_config.row_anchors
+            lane = np.stack([lane_xs, lane_ys], axis=1)
+            lane_type = lane_config.lane_classes[lane_dict['class']]
             color = pygame.Color(0, 0, 0)
-            color.hsla = (hue, 95, 55, 100)
-
-            pygame.draw.circle(display, color, list(self.cam_pts[n]), 3)
-
+            color.hsla = (hue, 90 + np.random.rand() * 10, 50 + np.random.rand() * 10, 100)
+            hue += hue_step
+            pygame.draw.circle(display, color, lane[0], 3)
+            for i in range(1, len(lane)):
+                if lane_xs[i] != -2:
+                    pygame.draw.circle(display, color, lane[i], 3)
+                    if lane_xs[i-1] != -2:
+                        pygame.draw.line(display, color, lane[i-1], lane[i], 2)
+         
+    def group_lanes(self, waypoints):
+        lanes = {}
+        for wp in waypoints:
+            if (wp.lane_id, wp.road_id) not in lanes:
+                lanes[(wp.lane_id, wp.road_id)] = [wp]
+            else:
+                lanes[(wp.lane_id, wp.road_id)].append(wp)
+        return lanes
 
     def update(self):
-        # Convert all points to camera coords and remove ones outside of camera view
+        # Filter waypoints outside of camera view
         self.world2vehicle = np.array(self.vehicle.get_transform().get_inverse_matrix())
-        self.cam_pts, cam_pts_mask = self.project_to_camera(self.waypoints_pos)
+        sensor_points = self.transform_world_to_camera(self.waypoints_pos)
+        
+        # Get mask for all points within the max range threshold
+        cam_pts_mask = (np.sqrt(sensor_points[0]**2 + sensor_points[1]**2 + sensor_points[2]**2) < self.max_waypoint_range)
+        
+        # Group lanes by lane id
+        filtered_waypoints = [wp for wp, in_cam in zip(self.waypoints, cam_pts_mask) if in_cam]
+        lanes_dict = self.group_lanes(filtered_waypoints)
 
-        # Get road and lane IDs for all camera points
-        self.cam_road_ids = self.waypoints_road_id[cam_pts_mask] # (M,)
-        self.cam_lane_ids = self.waypoints_lane_id[cam_pts_mask] # (M,)    
+        # Get lanes on both sides of the vehicle
+        ego_location = self.vehicle.get_location()
+        self.waypoint = self.map.get_waypoint(ego_location, project_to_road=True)
+        right_side_positive = self.waypoint.lane_id > 0
+
+        self.lanes = []
+        for (lane_id, road_id), lane_points in lanes_dict.items():
+            right_of_car = (lane_id > self.waypoint.lane_id) if right_side_positive else (lane_id < self.waypoint.lane_id)
+            right_side_of_road = (lane_id > 0) if right_side_positive else (lane_id < 0)
+            if lane_id == self.waypoint.lane_id: # current lane of car
+                self.get_marking_positions(lane_points, False, Side.RIGHT)
+                self.get_marking_positions(lane_points, False, Side.LEFT)
+            elif right_of_car: # on right side of car
+                self.get_marking_positions(lane_points, False, Side.RIGHT)
+            elif right_side_of_road: # right side of road and on left side of car
+                self.get_marking_positions(lane_points, False, Side.LEFT)
+            else: # opposite side of road and on left side of car
+                self.get_marking_positions(lane_points, True, Side.LEFT)
+
